9f9203060e76d99158770fe63affefbca0da9b78
Unknown <fluffy1snow@gmail.com>
2019-04-18 18:26:11 +0300

    delete old projects

--- Angular/app/scripts/controllers/additionMain.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('angularApp')
-    .controller( 'additionMainCtrl', function ( $scope, $rootScope, $timeout, $state, $stateParams, $http, $cookies, model ) {
-        $rootScope.slider = 'next';        
-    });
--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # samples
 Contains:
-- Angular example
-- Some random landing page sample
 - React-Redux production project example
-- VanillaJS project example
